15ff9c99b9be69c2dbb22b2b12514d14e95a2a80
fap <459631+fapdash@users.noreply.github.com>
2022-10-14 19:40:54 +0200

    Add exploratory test cases
    for exercism/exercism#6557

--- test/commands/user/link_with_github.rb
+++ test/commands/user/link_with_github.rb
@@ -18,4 +18,40 @@ class User::LinkWithGithubTest < ActiveSupport::TestCase
     assert_equal provider, user.provider
     assert_equal uid, user.uid
   end
+
+  test "changes github_username" do
+    provider = 'github'
+    uid = '111'
+    auth = stub(
+      provider:,
+      uid:,
+      info: stub(nickname: "fapdash")
+    )
+    user = create :user, provider: "github", uid: "111", email: "example@email.com", github_username: "fap-"
+
+    User::LinkWithGithub.(user, auth)
+
+    assert_equal "fapdash", user.github_username
+    assert_equal uid, user.uid
+    assert_equal provider, user.provider
+  end
+
+  test "github_username already associated with account" do
+    provider = 'github'
+    uid = '111'
+    auth = stub(
+      provider:,
+      uid:,
+      info: stub(nickname: "fapdash")
+    )
+    user = create :user, provider: "github", uid: "111", email: "example@email.com", github_username: "fap-"
+    user_2 = create :user, provider: "github", uid: "112", email: "example2@email.com", github_username: "fapdash"
+
+    User::LinkWithGithub.(user, auth)
+
+    assert_equal "fapdash", user.github_username # how? why? should be unique?
+    assert_equal "fapdash", user_2.github_username
+    assert_equal uid, user.uid
+    assert_equal provider, user.provider
+  end
 end
